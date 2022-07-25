b1aff0f1407bdabead715cc66badf53674ea73d4
SamLinux <oxedom@gmail.com>
2022-07-25 22:38:45 +0300

    input and cleaned up POC from JS

--- app.js
+++ app.js
@@ -1,7 +1,4 @@
-// const game = (() => {
-// })()
-
-    const Player = function (name, playerSymbol, icon) {
+ const Player = function (name, icon, avatar) {
         'use strict';
     
         let _score = 0;
@@ -9,8 +6,8 @@
         let _name = name;
     
         let _propObj = {
-            playerSymbol,
             icon,
+            avatar,
             score: _score,
             moves: _moves,
             name: _name,
@@ -42,63 +39,79 @@
         };
     };
 
-
-
-
-
 const jeff = Player('jeff', '1337', '666')
-console.log(jeff);
 
 
 
+const dom = (() => {
+        'use strict';
 
-// const dom = (() => {
-//     'use strict';
+        //Private Vars
+        const _form = document.getElementById("form")
 
-//     //Private Vars
-//     const _form = document.getElementById("form")
-//     let _privateProp = "Big Dog"
-//     let _privateProp2 = "Big Cat"
 
-//     //Public Vars
-//     let publicProp = _privateProp
-//     let publicProp2 = _privateProp2
+        
+       
+        let _privateProp = "Big Dog"
+        let _privateProp2 = "Big Cat"
+        // console.log('1')
+        //Public Vars
+        let publicProp = _privateProp
+        let publicProp2 = _privateProp2
+        
+    
+    
+        //Private Methods
+        let _privateFunction = () => {
+            console.log([1, 2, 3, 4])
+            return ['one,two'];
+        }
+    
+        //Public Methods
+        let publicFunction = () => {
+            return _privateFunction()
+        }
+        
+        let renderScore = () => {    
+         
+           
+
+        }
+    
+        //TEST
+        _form.addEventListener("submit", (e) => {
+            e.preventDefault()
+            console.log(e.target);
+            const formData = new FormData(e.target)
+            const formProps = Object.fromEntries(formData)
+            console.log(formProps)
+    })
+        //Returning Moudles Methods and Props
+        return {
+            publicProp,
+            publicProp2,
+            publicFunction,
+            renderScore
+        }
+
+     
+        
+    
+    })();
+    
     
 
-
-//     //Private Methods
-//     let _privateFunction = () => {
-//         console.log([1, 2, 3, 4])
-//         return ['one,two'];
-//     }
-
-//     //Public Methods
-//     let publicFunction = () => {
-//         return _privateFunction()
-//     }
-//     //Returning Moudles Methods and Props
-//     return {
-//         publicProp,
-//         publicProp2,
-//         publicFunction,
-//         ass: "dog"
-//     }
-
-// })();
-
-// console.log(dom.publicFunction());
-
-
-// console.log(form);
-// //TEST
-// form.addEventListener("submit", (e) => {
-//     e.preventDefault()
-//     console.log(e.target);
-//     const formData = new FormData(e.target)
-//     const formProps = Object.fromEntries(formData)
-//     console.log(formProps);
+dom.renderScore()
+dom.renderScore()
+dom.renderScore()
 
 
 
 
-// })
+
+
+
+
+
+
+
